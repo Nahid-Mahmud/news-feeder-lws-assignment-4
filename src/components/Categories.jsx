59bfebba19190229192d1c77c590db543ab1dd4c
Md. Nahid Mahmud <nahidmahmudn@gmail.com>
2024-07-29 15:16:25 +0600

    categories options animathion added on change

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -12,44 +12,58 @@ const Categories = () => {
     <div className="container mx-auto   mt-6">
       <ul className="flex flex-wrap items-center justify-center gap-5 text-xs font-semibold lg:text-base">
         <li
-          className={`cursor-pointer ${category === "general" ? "text-green-700" : ""}`}
+          className={`cursor-pointer transition-all duration-300 ${
+            category === "general" ? "text-green-700 scale-105 font-bold" : ""
+          }`}
           onClick={() => handleClick("General")}
         >
           General
         </li>
         <li
-          className={`cursor-pointer ${category === "business" ? "text-green-700" : ""} `}
+          className={`cursor-pointer transition-all duration-300 ${
+            category === "business" ? "text-green-700 scale-105 font-bold" : ""
+          } `}
           onClick={() => handleClick("Business")}
         >
           Business
         </li>
         <li
-          className={`cursor-pointer ${category === "entertainment" ? "text-green-700" : ""} 
+          className={`cursor-pointer transition-all duration-300 ${
+            category === "entertainment" ? "text-green-700 scale-105 font-bold" : ""
+          } 
         }`}
           onClick={() => handleClick("Entertainment")}
         >
           Entertainment
         </li>
         <li
-          className={`cursor-pointer ${category === "health" ? "text-green-700" : ""}`}
+          className={`cursor-pointer transition-all duration-300 ${
+            category === "health" ? "text-green-700 scale-105 font-bold" : ""
+          }`}
           onClick={() => handleClick("Health")}
         >
           Health
         </li>
         <li
-          className={`cursor-pointer ${category === "science" ? "text-green-700" : ""}`}
+          className={`cursor-pointer transition-all duration-300 ${
+            category === "science" ? "text-green-700 scale-105 font-bold" : ""
+          }`}
           onClick={() => handleClick("Science")}
         >
           Science
         </li>
         <li
-          className={`cursor-pointer ${category === "sports" ? "text-green-700" : ""}`}
+          className={`cursor-pointer transition-all duration-300 ${
+            category === "sports" ? "text-green-700 scale-105 font-bold" : ""
+          }`}
           onClick={() => handleClick("Sports")}
         >
           Sports
         </li>
         <li
-          className={`cursor-pointer ${category === "technology" ? "text-green-700" : ""} 
+          className={`cursor-pointer transition-all duration-300 ${
+            category === "technology" ? "text-green-700 scale-105 font-bold" : ""
+          } 
         }`}
           onClick={() => handleClick("Technology")}
         >
